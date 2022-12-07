f5422fd7fc109e181b7ed053fe7f2037cab30eef
Danilo Duarte <danilo.duartejp@outlook.com>
2022-12-07 19:52:09 -0300

    class 10

---
+++ Aula10.py
@@ -0,0 +1,60 @@
+class Graph():
+ 
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)]
+                      for row in range(vertices)]
+ 
+    def printSolution(self, dist, src, final):
+        print("Vertice inicial\t Vertice final \t Distância")
+        for node in range(self.V):
+            if(node == final):
+                print(src, "\t\t\t",node,"\t\t", dist[node])
+                break
+ 
+    def minDistance(self, dist, sptSet):
+ 
+        min = 1e7
+ 
+        for v in range(self.V):
+            if dist[v] < min and sptSet[v] == False:
+                min = dist[v]
+                min_index = v
+ 
+        return min_index
+    
+    def dijkstra(self, src, final):
+ 
+        dist = [1e7] * self.V
+        dist[src] = 0
+        sptSet = [False] * self.V
+ 
+        for cout in range(self.V):
+ 
+            u = self.minDistance(dist, sptSet)
+ 
+            sptSet[u] = True
+ 
+            for v in range(self.V):
+                if (self.graph[u][v] > 0 and
+                   sptSet[v] == False and
+                   dist[v] > dist[u] + self.graph[u][v]):
+                    dist[v] = dist[u] + self.graph[u][v]
+ 
+        self.printSolution(dist, src, final)
+
+g = Graph(9)
+g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
+           [4, 0, 8, 0, 0, 0, 0, 11, 0],
+           [0, 8, 0, 7, 0, 4, 0, 0, 2],
+           [0, 0, 7, 0, 9, 14, 0, 0, 0],
+           [0, 0, 0, 9, 0, 10, 0, 0, 0],
+           [0, 0, 4, 14, 10, 0, 2, 0, 0],
+           [0, 0, 0, 0, 0, 2, 0, 1, 6],
+           [8, 11, 0, 0, 0, 0, 1, 0, 7],
+           [0, 0, 2, 0, 0, 0, 6, 7, 0]
+           ]
+ 
+src = 1
+final = 8
+g.dijkstra(src, final)
